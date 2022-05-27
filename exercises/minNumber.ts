855e8aa57a9fd114da2f50cc38bfb59a93e813f7
loicbdev <loicbodolec@msn.com>
2022-05-27 08:49:13 +0200

    add: solutions

--- exercises/minNumber.ts
+++ exercises/minNumber.ts
@@ -1,3 +1,13 @@
-function minNumber(numbers: number[]): number {}
+function minNumber(numbers: number[]): number {
+  if (numbers.length == 1) {
+    return numbers[0];
+  }
+  const prevMin = minNumber(numbers.slice(1));
+  const currentNb = numbers[0];
+
+  if (currentNb < prevMin) {
+    return currentNb;
+  } else return prevMin;
+}
 
 export default minNumber;
--- exercises/palindrome.ts
+++ exercises/palindrome.ts
@@ -1,3 +1,21 @@
-function palindrome(word: string): boolean {}
+function palindrome(word: string): boolean {
+  // console.log(">> palindrome", word);
+  if (word.length <= 1) {
+    return true;
+  }
+  const compare = word[0] === word[word.length - 1];
+  if (!compare) return false;
+  // console.log(
+  //   "first&last letter equal?",
+  //   word[0],
+  //   word[word.length - 1],
+  //   compare
+  // );
+  const res = palindrome(word.slice(1, -1));
+
+  return res;
+}
+
+console.log(palindrome("ressassez"));
 
 export default palindrome;
